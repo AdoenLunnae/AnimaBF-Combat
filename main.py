98beb6315fbfc15fd726de6ec9677bf3b0d452ea
AdoenLunnae <unshekdelmonton@gmail.com>
2018-08-23 01:10:08 +0200

    Basic funcionality

---
+++ main.py
@@ -0,0 +1,88 @@
+# -*-coding:utf-8 -*-
+
+import math
+import tkinter as tk
+from tkinter import ttk
+
+
+class App():
+    def __init__(self):
+        self.raiz = tk.Tk()
+        self.raiz.title("AnimaCombatHelper")
+        self.raiz.resizable(0, 0)
+        self.raiz.configure(bg='gray')
+
+        atqframe = ttk.LabelFrame(self.raiz, text='Atacante')
+        defframe = ttk.LabelFrame(self.raiz, text='Defensor')
+        arolllabel = ttk.Label(atqframe, text='Tirada')
+        atqlabel = ttk.Label(atqframe, text='Hab. Ataque')
+        dmglabel = ttk.Label(atqframe, text=u'Daño base')
+        drolllabel = ttk.Label(defframe, text='Tirada')
+        deflabel = ttk.Label(defframe, text='Hab. Defensa')
+        talabel = ttk.Label(defframe, text='TA')
+
+        aroll, atq, dmg, droll, deff, ta = tk.StringVar(), tk.StringVar(), tk.StringVar(), tk.StringVar(), \
+            tk.StringVar(), tk.StringVar()
+        aroll.set(0)
+        droll.set(0)
+        atq.set(0)
+        deff.set(0)
+        dmg.set(0)
+        arollbox = ttk.Entry(atqframe, width=12, textvariable=aroll)
+        drollbox = ttk.Entry(defframe, width=12, textvariable=droll)
+        atqbox = ttk.Entry(atqframe, width=12, textvariable=atq)
+        defbox = ttk.Entry(defframe, width=12, textvariable=deff)
+        dmgbox = ttk.Entry(atqframe, width=12, textvariable=dmg)
+        taselect = ttk.Combobox(defframe, width=10, textvariable=ta)
+        taselect['values'] = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+        taselect.current(0)
+
+        atqframe.grid(column=0, row=0)
+        defframe.grid(column=0, row=1)
+
+        arolllabel.grid(column=0, row=0)
+        atqlabel.grid(column=1, row=0)
+        dmglabel.grid(column=2, row=0)
+        arollbox.grid(column=0, row=1)
+        atqbox.grid(column=1, row=1)
+        dmgbox.grid(column=2, row=1)
+
+        drolllabel.grid(column=0, row=0)
+        deflabel.grid(column=1, row=0)
+        talabel.grid(column=2, row=0)
+        drollbox.grid(column=0, row=1)
+        defbox.grid(column=1, row=1)
+        taselect.grid(column=2, row=1)
+        final = ttk.Label(self.raiz, text='0')
+        final.grid(column=0, row=2)
+        while 1:
+            try:
+                final.configure(text=self.getdmg(int(atq.get()), int(aroll.get()), int(dmg.get()),
+                                                          int(deff.get()), int(droll.get()), int(ta.get())))
+            except ValueError:
+                final.configure(text=0)
+            self.raiz.update_idletasks()
+            self.raiz.update()
+
+    def getdmg(self, atq, aroll, base, deff, droll, ta):
+        diff = 10*math.trunc((atq+aroll-deff-droll)/10)
+        if diff > 0:
+            abso = 20+10*ta
+            diff -= abso
+            dmg = base*diff//100
+            if dmg > 0:
+                return '{}%. {} de daño'.format(diff, dmg)
+            else:
+                return 'A la defensiva'
+        elif diff < 0:
+            contr = -diff//2
+            return 'Contraataque con +{}'.format(contr)
+
+
+def main():
+    window = App()
+    return 0
+
+
+if __name__ == '__main__':
+    main()

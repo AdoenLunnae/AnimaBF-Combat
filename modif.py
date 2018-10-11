0b3316f5ec5756ab2a48505056f66d2ce10dfef2
AdoenLunnae <unshekdelmonton@gmail.com>
2018-10-11 11:01:46 +0200

    Added list of modifiers

---
+++ modif.py
@@ -0,0 +1,19 @@
+modifatk = {'A la defensiva': -30, 'A la ofensiva': 10, 'Adversario diminuto': -20, 'Adversario pequeño': -10,
+            'Amenazado': -20, 'Ataque total': 30, 'Cargando': 10, 'Ceguera completa': -100, 'Ceguera parcial': -30,
+            'Derribado': -30, 'Desenfundado': -30, 'Dolor': -40, 'Dolor extremo': -80, 'Espacio reducido': -40,
+            'Espalda': -30, 'Flanco': -10, 'Levitando': -20, 'Miedo': -60, u'Parálisis completa': -200,
+            u'Parálisis menor': -20, u'Parálisis parcial': -80, u'Posición superior': 20, 'Vuelo 7-14': 10,
+            'Vuelo 15+': 10}
+
+modifdef = {'A la defensiva': 10, 'A la ofensiva': -30, u'Adversario diminuto(Parada)': -10, "Aguantar el golpe": -80,
+            "Amenazado": -100, u'Ataque en área(Esquiva)': -80, u'Ataque en área(Parada)': -120,
+            'Cargando(Esquiva)': -20, 'Cargando(Parada)': -10, 'Ceguera absoluta': -80, 'Ceguera Parcial(Esquiva)': -10,
+            'Ceguera Parcial(Parada)': -30, 'Defensa Total': 30, 'Derribado': -30, 'Desenfundado': -20, 'Dolor': -40,
+            'Dolor Extremo': -80, 'Espacio Reducido': -20, 'Espalda': -80, 'Flanco': -30, 'Levitando(Esquiva)': -40,
+            'Levitando(Parada)': -20, 'Miedo': -60, u'Parálisis completa': -200, u'Parál. Menor(Esquiva)': -40,
+            u'Parál. Menor(Parada)': -20, u'Parálisis Parcial': -80, 'Proyectil Disparado(Esquiva)': -30,
+            'Proyectil Disparado(Parada)': -80, u'Proyectil Disparado(Maestría Parada)': -20,
+            'Proyectil Disparado(Escudo)': -20, 'Proyectil Lanzado(Parada)': -50, 'Sorpresa': -90, 'Vuelo 7-14': 10,
+            'Vuelo 15+(Esquiva)': 20, 'Vuelo 15+(Parada)': 10}
+
+print(len(modifdef.keys()))
